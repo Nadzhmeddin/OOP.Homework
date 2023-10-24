e07ff2a32468908e22c941138e9721035783f0fc
Nadzhmeddin <xside55@yandex.ru>
2023-10-24 13:26:16 +0300

    HW2

---
+++ Homework_2/Account.java
@@ -0,0 +1,34 @@
+package Homework_1;
+
+
+public class Account {
+
+    protected  double balance;
+    
+
+    Account(double balance) {
+        this.balance = balance; 
+    }
+
+    public void putMoney(double money) {
+            this.balance += money;
+            System.out.printf("Ваш текущий баланс: %s", balance); 
+    }
+    
+    public void getMoney(double money) {
+        if(balance >= money) {
+            double res = balance - money;
+            balance = res;
+            System.out.printf("Ваш текущий баланс: %s", balance);
+        }
+        else System.out.println("Недостаточно средств на счете");
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
